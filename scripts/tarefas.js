ed7db634b5478c69f4eee036b1eac8b95276f721
Thais S Lino <thaisslino@gmail.com>
2022-07-12 21:47:51 -0400

    adiciona funcao de getTask

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,3 +1,7 @@
+const apiUrl = 'https://ctd-fe2-todo-v2.herokuapp.com'
+const createTaskButtonElement = document.querySelector('#createTaskButton')
+const skeletonElement = document.querySelector('#skeleton')
+
 function logout() {
     window.location.href = "./index.html"
     // AO FAZER LOGOUT REMOVER DADOS DO SESSION STORAGE 
@@ -42,28 +46,109 @@ function usuarioLoad() {
     getListTarefas()
 }
 
+//============================================
+//     Função para mostrar Nome do User
+//============================================
 
-// tem  que arrumar as datas pra padrao Brasil
+function getUserInfo() {
+
+    fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/users/getMe', { headers: headersAuthRequest }).then(
+
+        response => {
+
+            if(response.ok) {
+
+                response.json().then(
+
+                    user => {
+
+                        console.log(user)
+                        console.log(`${user.firstName} ${user.lastName}`)
+
+                        // !Insira a lógica aqui para mostrar o Nome Completo do usuário no HTML da Aplicação
+
+                    }
+
+                )
+
+            } else {
+
+                localStorage.clear()
+                window.location.href = './../index.html'
+
+            }
+
+        }
+
+    )
+
+}
+
+//============================================
+//       Função que Obtem as Tarefas
+//============================================
+function getTasks() {
+
+    fetch(`${apiUrl}/tasks`, { headers: headersAuthRequest }).then(
+
+        response => {
+
+            response.json().then(
+
+                tasks => {
+
+                    // Remoção dos itens que estavam antes dentro da Lista inicial
+                    listTasks.innerHTML = ''
+
+                    for(let task of tasks) {
+
+                        console.log(task)
+
+                        for(let task of tasks){
+
+                            `
+                                <li class="tarefa">
+                                    <div class="not-done"></div>
+                                    <div class="descricao">
+                                        <p class="nome">NovaTarefa</p>
+                                        <p class="timestamp">Criada em: 15/07/21</p>
+                                    </div>
+                                </li>
+                            `
+                        }
+
+                    }
+
+                }
+
+            )
+
+        }
+
+    )
+
+}
+
+if(token === null) {
+
+    window.location.href = './../index.html'
+} else {
+    getUserInfo()
+    getTasks()
+}
 
 
-
-
-
-
-
-// obter lista de tarefas
-
-// Função que Obtem as Tarefas
+//============================================
+//       Função que so deus sabe
+//============================================
 function getListTarefas() {
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlkIjo1MSwiaWF0IjoxNjU3NDk0MzAwfQ.FigzR5SsyZrec6hgVkrNYtIXZvT-49hQ0qRfvV7uuOU"
-
     const headersAuthRequest = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': token
     }
 
-    fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', { headers: headersAuthRequest }).then(
+    fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/tasks', { headers: headersAuthRequest }).then(
         response => {
             response.json().then(
                 tasks => {
@@ -97,3 +182,66 @@ token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0cmluZyIsImlkIjo1MS
         }
     )
 }
+
+
+//============================================
+//       Função que cria uma task
+//============================================
+function createTask() {
+
+    // Objeto que será enviado para a API
+    let data = {
+        description: 'Tarefa Teste',
+        completed: false
+    }
+
+    // Objeto que servira como Configuração da Requisição de POST
+    let postRequestConfiguration = {
+        method: 'POST',
+        headers: headersAuthRequest,
+        body: JSON.stringify(data)
+    }
+
+    fetch(`${apiUrl}/tasks`, postRequestConfiguration).then(
+
+        response => {
+
+            if(response.ok) {
+
+                // !Inserir Lógica para obter as Tarefas Novamente
+
+            }
+
+        }
+
+    )
+
+}
+
+
+//============================================
+//  Event Listener do Botão para criar Task
+//============================================
+createTaskButtonElement.addEventListener('click', event => {
+
+    event.preventDefault()
+
+    // Chama a função que Cria uma Tarefa
+    createTask()
+
+})
+
+//============================================
+//         Datas Padrão Brasil
+//============================================
+let dataCriacao = new Date(task.createdAt)
+let dataCriacaoFormatada = dataCriacao.toLocaleDateString(
+    'pt-BR',
+    {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    }
+)
+
+// parei a aula de 05/07 as 53:02
